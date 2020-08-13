f09812a292ac7b073b58db58618b31a46a4127c6
Subhadeep79 <67587371+Subhadeep79@users.noreply.github.com>
2020-08-13 19:07:33 +0530

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,16 @@
+#reading_data
+##file is stored in my working directory
+power<- read.table("household_power_consumption.txt", skip=1, sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- power[power$Date %in% c("1/2/2007","2/2/2007") ,] ##new data set 
+
+subMetering1 <- as.numeric(subpower$Sub_metering_1)
+subMetering2 <- as.numeric(subpower$Sub_metering_2)
+subMetering3 <- as.numeric(subpower$Sub_metering_3)
+
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=3, col=c("black", "red", "blue"))
+dev.off()
